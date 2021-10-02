713164c70cff3464e2cab73fd2d718d7b9792898
Juan Lucero <jlucero@alphaworks.tech>
2021-10-01 23:30:45 -0700

    Added simple layout

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [itemCount, setItemCount] = useState(2);
+  const [imtes, setItems] = useState({
+    1: 'first item',
+    2: 'second item',
+  });
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View
+        style={styles.blue}
+      >
+        <Text
+          style={styles.menuLink}
+          onPress={() => console.log('home pressed')}
+        >
+          Home
+        </Text>
+        <Text
+          style={styles.menuLink}
+          onPress={() => console.log('about pressed')}
+        >
+          About
+        </Text>
+        <Text
+          style={styles.menuLink}
+          onPress={() => console.log('contact pressed')}
+        >
+          Contact
+        </Text>
+      </View>
+      <View
+        style={styles.orange}
+      >
+        <Text>Juan</Text>
+      </View>
+      <View
+        style={styles.yellow}
+      >
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +50,33 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    display: 'flex',
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  blue: {
+    width: '100%',
+    height: 100,
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'row',
+  },
+  orange: {
+    width: '100%',
+    height: 'calc(100% - 150px)',
+    backgroundColor: 'orange',
+    padding: 30,
+  },
+  yellow: {
+    width: '100%',
+    height: 50,
+    backgroundColor: 'yellow',
+  },
+  menuLink: {
+    color: 'white',
+  }
 });
